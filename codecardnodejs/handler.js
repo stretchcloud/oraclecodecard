cf152f511862586dfdef52bdb4ace5952e64e781
jit2600@gmail.com <jit2600@gmail.com>
2019-05-01 14:37:24 +0100

    final code commit

--- codecardnodejs/handler.js
+++ codecardnodejs/handler.js
@@ -1,5 +1,15 @@
 "use strict"
 
+var codeJson = {
+
+      "template": "template4",
+      "title": "Prasenjit Sarkar",
+      "subtitle": "Cloud Native Evangelist",
+      "bodytext": "Ask me anything on Cloud Native Application World! I run blog.kube-mesh.io",
+      "icon": "microservices",
+      "backgroundColor": "white"
+};
+
 module.exports = (context, callback) => {
-    callback(undefined, {status: "done"});
+    callback(undefined, codeJson);
 }
